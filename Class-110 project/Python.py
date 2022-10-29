8c538e1e88d38ff8108cfa59189352055175ba31
amangorach <91610050+amangorach@users.noreply.github.com>
2022-10-29 15:44:38 +0530

    Add files via upload

---
+++ Class-110 project/Python.py
@@ -0,0 +1,41 @@
+import random
+# I was not able to do a random int thing
+rand = int(input("please input a number from 1-6"))
+for i in rand:
+    if rand == 1:
+        print("_____")
+        print("     ")
+        print("  0  ")
+        print("     ")
+        print("_____")
+    elif rand == 2:
+        print("_____")
+        print("     ")
+        print(" 0 0 ")
+        print("     ")
+        print("_____")
+    elif rand == 3:
+        print("_____")
+        print("     ")
+        print("0 0 0")
+        print("     ")
+        print("_____")
+    elif rand == 4:
+        print("_____")
+        print(" 0 0 ")
+        print("     ")
+        print(" 0 0 ")
+        print("_____")
+    elif rand == 5:
+        print("_____")
+        print(" 0 0 ")
+        print("  0  ")
+        print(" 0 0 ")
+        print("_____")
+    elif rand == 4:
+        print("_____")
+        print(" 0 0 ")
+        print(" 0 0 ")
+        print(" 0 0 ")
+        print("_____")
+print(i)
